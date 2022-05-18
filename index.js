500826e1d97b406a651a25f3aa7a80d81ca545b7
Stif <stifskere@gmail.com>
2022-05-18 04:45:35 +0200

    jamie

--- index.js
+++ index.js
@@ -52,3 +52,13 @@ client.on('messageCreate', async (message) => {
         await message.reply(answers[Math.floor(Math.random() * answers.length)]);
     }
 })
+
+client.on('guildMemberAdd', async (member) => {
+    const wlcEmbed = new MessageEmbed()
+        .setTitle(`Welcome ${member.user.username}`)
+        .setDescription('Check <#976150169542348842> or run /worship')
+        .setColor('RANDOM')
+        .setThumbnail('https://cdn.discordapp.com/avatars/394127601398054912/c7a08756f08ff4fa9f51cf5f63f017d0.png?size=4096')
+
+    member.guild.channels.cache.get('976149800447770628').send({embeds: [wlcEmbed]});
+})
