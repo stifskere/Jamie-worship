d1f7738400434e6b4adadeb7453b7c00f9bca115
Stif <stifskere@gmail.com>
2022-05-19 01:12:47 +0200

    jamie

--- index.js
+++ index.js
@@ -68,6 +68,17 @@ client.on('interactionCreate', async (interaction) => {
 
 client.on('messageCreate', async (message) => {
     if(message.author.bot) return;
+
+    if(message.author.id === '394127601398054912'){
+        const embed = new MessageEmbed()
+            .setTitle('Jamie said something')
+            .setDescription(`**He said:** ${message.content}`)
+            .setFooter({text: 'I\'l keep whatever our god says in here'})
+
+        message.guild.channels.cache.get('976149822476279929').send({embeds: [embed]})
+        return;
+    }
+
     if(message.content.toLowerCase().includes('jamie')){
         const answers = ["Want to talk about jamie with me?", "Did you say jamie?", "I also like Jamie", "Jamie is a god for me", "You talking about jamie and not telling me!?"];
         await message.reply(answers[Math.floor(Math.random() * answers.length)]);
