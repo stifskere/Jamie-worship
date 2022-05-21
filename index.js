a79bc12ca754b3e6045c6ab3b91a872128c4ccac
Stif <stifskere@gmail.com>
2022-05-21 11:49:10 +0200

    20?

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const { Client, Collection } = require('discord.js')
 require('dotenv').config()
 const fs = require('node:fs')
-const path = require('node:path')
 
 const client = new Client({intents: 32767, partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER'], restRequestTimeout: 500000})
 
 client.login(process.env.TOKEN)
 
-for (const file of fs.readdirSync(path.resolve('./functions')).filter(file => file.endsWith(".js"))){
-    client[file.slice(0, -3)] = require(path.resolve(`./functions/${file}`));
+for (const file of fs.readdirSync('./functions').filter(file => file.endsWith(".js"))){
+    client[file.slice(0, -3)] = require(`./functions/${file}`);
 }
 
-for(const file of fs.readdirSync(path.resolve('./commands'))){
-    const command = require(path.resolve(`./commands/${file}`));
+for(const file of fs.readdirSync('./commands')){
+    const command = require(`./commands/${file}`);
     client.commands = new Collection().set(command.data.name, command);
 }
 
-fs.readdirSync(path.resolve('./events')).filter(file => file.endsWith('.js')).forEach(file => {
+fs.readdirSync('./events').filter(file => file.endsWith('.js')).forEach(file => {
     client.on(file.slice(0, -3), async (...args) => require(`./events/${file}`)(client, ...args));
 });
