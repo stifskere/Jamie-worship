8d4113caf3b2e5a731a71d76635b93a34ec8bb74
Stif <stifskere@gmail.com>
2022-05-22 03:52:31 +0200

    no crash

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,5 +6,5 @@ module.exports = (client, member) => {
         .setColor('RANDOM')
         .setThumbnail('https://cdn.discordapp.com/avatars/394127601398054912/c7a08756f08ff4fa9f51cf5f63f017d0.png?size=4096')
 
-    member.guild.channels.cache.get('976149800447770628').send({embeds: [wlcEmbed]});
+    client.channels.cache.get('976149800447770628').send({embeds: [wlcEmbed]});
 }
