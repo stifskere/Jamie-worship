0b690e54d49c57d6a30e6ea54d2e418feee52240
Stif <stifskere@gmail.com>
2022-05-25 16:06:37 +0200

    troll

--- sharding.js
+++ sharding.js
@@ -2,9 +2,15 @@ const { ShardingManager } = require('discord.js');
 require('dotenv').config();
 require('@memw/betterconsole')();
 
-const manager = new ShardingManager('./index.js', { token: process.env.TOKEN });
+const manager = new ShardingManager('./index.js', {
+    token: process.env.TOKEN,
+    respawn: true
+});
 
 (async () => {
-    manager.on('shardCreate', shard => console.info(`Launched shard ${shard.id}`));
+    manager.on('shardCreate', shard => {
+        console.info(`Launched shard ${shard.id}`)
+    });
+
     await manager.spawn();
 })();
