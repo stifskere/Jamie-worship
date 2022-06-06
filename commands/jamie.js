2443e7b83cc07486bf14e3e51ac3e8ec671fde97
Stif <stifskere@gmail.com>
2022-06-06 22:53:27 +0200

    pfpfpfpf

--- commands/jamie.js
+++ commands/jamie.js
@@ -10,47 +10,50 @@ module.exports = {
         .addSubcommand(photo => photo
             .setName('photo')
             .setDescription('sends the Jamie photo')),
-    async execute(interaction){
+    async execute(interaction, client){
         await interaction.deferReply();
 
         const closeFriends = [];
 
-        let role = interaction.guild.roles.cache.get('976175903371571220')
+        let role = client.guilds.cache.get('976149800447770624').roles.cache.get('976175903371571220')
+        let interactionGuildPromise = await interaction.guild.members.fetch()
 
         if(typeof role === "object"){
-            role.members.map(member => {
-                closeFriends.push(member.user.tag)
+            role.members.map(async member => {
+                if(typeof await interactionGuildPromise.find(f => f.id === member.id) === 'object') closeFriends.push(member.user.tag)
             })
         }
 
-        const jamieObject = interaction.guild.members.cache.get("394127601398054912").user
-        if(interaction.options.getSubcommand() === 'profile'){
-            const jamie = {
-                heIS: (typeof jamieObject === "object"),
-                tag: (typeof jamieObject === "object") ? jamieObject.tag : "Jamie#8409",
-                id: (typeof jamieObject === "object") ? jamieObject.id : "394127601398054912",
-                pfp: (typeof jamieObject === "object") ? jamieObject.avatarURL() : "https://cdn.discordapp.com/avatars/394127601398054912/c7a08756f08ff4fa9f51cf5f63f017d0.png?size=4096"
+        setTimeout(async () => {
+            const jamieObject = interaction.guild.members.cache.get("394127601398054912").user
+            if(interaction.options.getSubcommand() === 'profile'){
+                const jamie = {
+                    heIS: (typeof jamieObject === "object"),
+                    tag: (typeof jamieObject === "object") ? jamieObject.tag : "Jamie#8409",
+                    id: (typeof jamieObject === "object") ? jamieObject.id : "394127601398054912",
+                    pfp: (typeof jamieObject === "object") ? jamieObject.avatarURL() : "https://cdn.discordapp.com/avatars/394127601398054912/c7a08756f08ff4fa9f51cf5f63f017d0.png?size=4096"
+                }
+
+                const embed = new MessageEmbed()
+                    .setTitle('Jamie')
+                    .setDescription(`**Jamie\'s tag:** ${jamie.tag}\n**Jamie\'s id:** ${jamie.id}\n\nHe lives in UK even tho he says he doesn\'t`)
+                    .addField('Jamie\'s close friends', (closeFriends.length !== 0) ? closeFriends.toString().replaceAll(',', '\n') : "No close friends on this server.")
+                    .addField('Total commits in SCP:ET github repo', '2')
+                    .setThumbnail(jamie.pfp)
+                    .setColor('RANDOM')
+
+                if(jamie.heIS === false) embed.setFooter({text: 'The info may not be up to date since the user can\'t be fetched.'})
+
+                interaction.editReply({embeds: [embed]})
+            }else if(interaction.options.getSubcommand() === 'photo'){
+                try{
+                    const photo = new MessageAttachment('./images/helloJamie.gif', 'helloJamie.gif')
+                    await interaction.channel.send({files: [photo]})
+                    interaction.deleteReply();
+                }catch{
+                    interaction.editReply({content: `Can't send images in this channel`})
+                }
             }
-
-            const embed = new MessageEmbed()
-                .setTitle('Jamie')
-                .setDescription(`**Jamie\'s tag:** ${jamie.tag}\n**Jamie\'s id:** ${jamie.id}\n\nHe lives in UK even tho he says he doesn\'t`)
-                .addField('Jamie\'s close friends', (closeFriends.length !== 0) ? closeFriends.toString().replaceAll(',', '\n') : "No close friends on this server.")
-                .addField('Dev hours on SCP:ET', '-5')
-                .setThumbnail(jamie.pfp)
-                .setColor('RANDOM')
-
-            if(jamie.heIS === false) embed.setFooter({text: 'The info may not be up to date since the user can\'t be fetched.'})
-
-            interaction.editReply({embeds: [embed]})
-        }else if(interaction.options.getSubcommand() === 'photo'){
-            try{
-                const photo = new MessageAttachment('./images/helloJamie.gif', 'helloJamie.gif')
-                await interaction.channel.send({files: [photo]})
-                interaction.deleteReply();
-            }catch{
-                interaction.editReply({content: `Can't send images in this channel`})
-            }
-        }
+        }, 500)
     }
 }
