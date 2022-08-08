0eab927295b5e94feb9d17686f571b0a76a29407
Memw <stifskere@gmail.com>
2022-08-08 08:42:19 +0200

    this should solve the error

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,16 +1,12 @@
 module.exports = async (client, interaction) => {
     if(!interaction) console.error('Unknown interaction, couldn\'t reply to the interaction')
-
     if(!interaction.inGuild()) return interaction.reply({content: 'No worshipping in DM allowed.', ephemeral: true});
-
     const command = client.commands.get(interaction.commandName);
-
     if(!command) return;
-
     if(command.disabled === true) return interaction.reply({content: 'This command is disabled.', ephemeral: true});
-
-        await command.execute(interaction, client);
-        client.botStats.commandUsage[interaction.commandName] = (client.botStats.commandUsage[interaction.commandName] || 0) + 1;
-        client.botStats.commandCount++;
-
+    await command.execute(interaction, client);
+    let interactionOptions;
+    try{interactionOptions = interaction.options.getSubcommand();}catch{interactionOptions = null;}
+    client.botStats.commandUsage[`${interaction.commandName}${interactionOptions ? interactionOptions : ""}`] = (client.botStats.commandUsage[`${interaction.commandName}${interactionOptions ? interactionOptions : ""}`] || 0) + 1;
+    client.botStats.commandCount++;
 }
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,17 +3,17 @@ module.exports = async (client, message) => {
     if(message.author.bot) return;
 
     if(message.author.id === '394127601398054912'){
-        const buttonActionRow = new MessageActionRow().addComponents(new MessageButton().setStyle('LINK').setLabel("Go to message").setURL(message.url))
+        const buttonActionRow = new MessageActionRow().addComponents(new MessageButton().setStyle('LINK').setLabel("Go to message").setURL(message.url));
 
         const embed = new MessageEmbed()
             .setTitle('Jamie said something')
             .setDescription(`**He said:** ${message.content}\n**In:** ${message.guild.name}`)
             .setFooter({text: 'I\'l keep whatever our god says in here'});
 
-        client.db.run(`UPDATE JamieInfo SET Value = '${message}' WHERE Key = 'LastMessage'`)
+        client.db.run(`UPDATE JamieInfo SET Value = '${message.content.replace(/[`']/gm, "")}' WHERE Key = 'LastMessage'`);
 
         client.db.all(`SELECT * FROM JamieInfo WHERE Key = 'messageNum'`, (err, row) => {
-            client.db.run(`UPDATE JamieInfo SET Value = '${parseInt(row[0].Value) + 1}' WHERE Key = 'messageNum'`)
+            client.db.run(`UPDATE JamieInfo SET Value = '${parseInt(row[0].Value) + 1}' WHERE Key = 'messageNum'`);
         })
 
         client.guilds.cache.get('976149800447770624').channels.cache.get('976832701015420998').send({embeds: [embed], components: [buttonActionRow]});
