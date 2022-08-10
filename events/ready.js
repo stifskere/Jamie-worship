cdd9c97e2dd9a9dbc3cbec406f2c146085d42337
Memw <stifskere@gmail.com>
2022-08-10 15:10:47 +0200

    set status 1h

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = (client) => {
         if(i >= statuses.length) i = 0;
         try{client.user.setActivity(statuses[i], {type: 'WATCHING'});}catch{}
         i++;
-    }, 60000)
+    }, 3600000)
 
     childProcess.exec('node register.js', async (err, stdout) => {
         if(err) return console.log(`There was an error on registering commands:\n${err}`);
