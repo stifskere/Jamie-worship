649c9fba2ee7e2bbb446a1017d95602f15b71ab2
Memw <stifskere@gmail.com>
2022-08-24 20:48:33 +0200

    No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie

--- commands/sendgif.js
+++ commands/sendgif.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require("@discordjs/builders");
+const {SlashCommandBuilder, EmbedBuilder} = require("@discordjs/builders");
 const {MessageAttachment} = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,11 +13,21 @@ module.exports = {
         const jamieObject = await client.users.fetch("394127601398054912")
         const photo = new MessageAttachment('./images/helloJamie.gif', 'helloJamie.gif')
 
+        const jamieEmbed = new EmbedBuilder()
+            .setTitle("Jamie gif sent to jamie")
+            .setDescription("Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie")
+            .setColor("RANDOM")
+
         try{
             jamieObject.send({content: `**From:** ${interaction.user.tag}`, files: [photo]})
-            await interaction.editReply({content: 'Your jamie gif was sent'})
+            await interaction.editReply({embeds: [jamieEmbed]})
         }catch{
-            await interaction.editReply({content: `There was an error on sending your jamie gif`})
+            jamieEmbed
+                .setTitle("Gif was not sent to jamie :(")
+                .setDescription("No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie")
+                .setFooter({text: "Not enough perms."})
+
+            await interaction.editReply({embeds: [jamieEmbed]})
         }
 
     }
