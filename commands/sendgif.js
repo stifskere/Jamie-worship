01a0d7e45a34ee239b08f875f3d6a4f21dfedaf5
Thunder <thundermaker300@gmail.com>
2022-08-24 14:40:48 -0400

    Update sendgif.js

--- commands/sendgif.js
+++ commands/sendgif.js
@@ -9,13 +9,13 @@ module.exports = {
             .setDescription('gif')),
 
     async execute(interaction, client){
-        await interaction.deferReply({ephemeral: true});
+        await interaction.deferReply();
         const jamieObject = await client.users.fetch("394127601398054912")
         const photo = new MessageAttachment('./images/helloJamie.gif', 'helloJamie.gif')
 
         try{
             jamieObject.send({content: `**From:** ${interaction.user.tag}`, files: [photo]})
-            await interaction.editReply({content: 'Your jamie gif was sent', ephemeral: true})
+            await interaction.editReply({content: 'Your jamie gif was sent'})
         }catch{
             await interaction.editReply({content: `There was an error on sending your jamie gif`})
         }
