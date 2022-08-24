cb1e3c291762c6e6c85f9f06e81ca57444977378
Memw <stifskere@gmail.com>
2022-08-24 20:49:48 +0200

    frfrfrfr ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

--- commands/sendgif.js
+++ commands/sendgif.js
@@ -10,24 +10,24 @@ module.exports = {
 
     async execute(interaction, client){
         await interaction.deferReply();
-        const jamieObject = await client.users.fetch("394127601398054912")
-        const photo = new MessageAttachment('./images/helloJamie.gif', 'helloJamie.gif')
+        const jamieObject = await client.users.fetch("394127601398054912");
+        const photo = new MessageAttachment('./images/helloJamie.gif', 'helloJamie.gif');
 
         const jamieEmbed = new EmbedBuilder()
             .setTitle("Jamie gif sent to jamie")
             .setDescription("Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie Jamie")
-            .setColor("RANDOM")
+            .setColor("RANDOM");
 
         try{
-            jamieObject.send({content: `**From:** ${interaction.user.tag}`, files: [photo]})
-            await interaction.editReply({embeds: [jamieEmbed]})
+            jamieObject.send({content: `**From:** ${interaction.user.tag}`, files: [photo]});
+            await interaction.editReply({embeds: [jamieEmbed]});
         }catch{
             jamieEmbed
                 .setTitle("Gif was not sent to jamie :(")
                 .setDescription("No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie No jamie")
-                .setFooter({text: "Not enough perms."})
+                .setFooter({text: "Not enough perms."});
 
-            await interaction.editReply({embeds: [jamieEmbed]})
+            await interaction.editReply({embeds: [jamieEmbed]});
         }
 
     }
