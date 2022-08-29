2711e509078f1bb17deaba8f92a80302875a9db0
Memw <stifskere@gmail.com>
2022-08-29 12:26:18 +0200

    dum thing

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -12,10 +12,10 @@ export default {
             const worshipersArray = [];
             for(let i in worshipersObject) worshipersArray.push([i, worshipersObject[i]]);
             worshipersArray.sort((a, b) => b[1] - a[1]);
-            const thisUserPos = worshipersArray.findIndex(x => x.id === interaction.user.id);
+            const thisUserPos = worshipersArray.findIndex(x => x[0] === interaction.user.id);
             const embed = new EmbedBuilder()
-                .setTitle("Leaderboard")
-                .setDescription("these are the top 5 worshipers who worshiped the most")
+                .setTitle("🏆 Leaderboard 🏆")
+                .setDescription("🙏 these are the top 5 worshipers who worshiped the most 🙏")
                 .setFooter({text: thisUserPos === -1 ? "You don't have any worship yet, use /worship to worship jamie" : `You are ${thisUserPos < 3 ? `${thisUserPos}st` : `${thisUserPos}th`} in a descending list of all the worshippers`})
                 .setColor(Math.floor(Math.random() * 0xFFFFFF));
             for(let i = 1; i <= 5; i++) embed.addFields({name: (i < 3 ? `${i}st. ` : `${i}th. `) +  client.getTag(await client.users.fetch(worshipersArray[i - 1][0])) ?? "Unknown user#0000", value: `**With:** ${worshipersArray[i - 1][1]} worships`});
