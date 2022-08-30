c1184103b6fc1321a65c7ff7eecedaf48fa42494
Memw <stifskere@gmail.com>
2022-08-30 12:45:36 +0200

    errors?

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,7 +11,10 @@ export default async (client, message) => {
             .setFooter({text: 'I\'l keep whatever our god says in here'});
 
         client.db.run(`UPDATE JamieInfo SET Value = '${message.content.replace(/[`']/gm, "")}' WHERE Key = 'LastMessage'`);
-        client.db.run(`UPDATE JamieInfo SET Value = Value + 1 WHERE Key = 'messageNum'`);
+
+        client.db.all(`SELECT * FROM JamieInfo WHERE Key = 'messageNum'`, (err, row) => {
+            client.db.run(`UPDATE JamieInfo SET Value = '${parseInt(row[0].Value) + 1}' WHERE Key = 'messageNum'`);
+        });
 
         client.guilds.cache.get('976149800447770624').channels.cache.get('976832701015420998').send({embeds: [embed], components: [buttonActionRow]});
         return;
