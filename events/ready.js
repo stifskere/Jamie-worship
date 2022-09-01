26255273a6d4de7f4b6ff42b4309a62406798e93
stifskere <stifskere@gmail.com>
2022-09-01 23:05:38 +0200

    This looks like a commit to me

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,13 @@
+import {ActivityType} from "discord-api-types/v10";
+
 export default async (client) => {
     const statuses = ["Whatever Jamie is doing", "Jamie", "Jamie again", "Jamie my beloved"]
 
-    let i = 0;
-
+    let statusInterval = 0;
     setInterval(async () => {
-        if(i >= statuses.length) i = 0;
-        await client.user.setActivity(statuses[i], {type: 'WATCHING'})
-        i++;
+        if(statusInterval >= statuses.length) statusInterval = 0;
+        await client.user.setPresence({activities: [{name: statuses[statusInterval], type: ActivityType.Watching}]});
+        statusInterval++;
     }, 3600000);
 
     await (await import("../register.js")).default();
