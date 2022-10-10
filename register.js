f4e4f1d8309fb4d9985a649e600dfa85e7998f08
Stif <stifskere@gmail.com>
2022-10-10 23:21:33 +0200

    This shit.... is getting out of my patience.

--- register.js
+++ register.js
@@ -3,22 +3,21 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
 (await import("dotenv")).config();
 
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = (await import(`./commands/${file}`)).default;
+    console.info(`Command loaded: ${command.data.name}`)
+    if(command.disabled) continue;
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
 export default async () => {
     try {
         console.info('started Registering slash commands');
-
-        const commands = [];
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-        for (const file of commandFiles) {
-            const command = (await import(`./commands/${file}`)).default;
-            console.info(`Command loaded: ${command.data.name}`)
-            if(command.disabled) continue;
-            commands.push(command.data.toJSON());
-        }
-
-        const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
 
         await rest.put(
             Routes.applicationCommands(process.env.APPID),
